302ece90560d803936a0512a86e3f8f31734ca91
Andrea Quiroga <andrea.quiroga99@gmail.com>
2022-04-18 20:29:23 -0300

    Entrega-2-Agregando-NavBar-Andrea-Quiroga

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 
+//import components
+import NavBar from './components/NavBar/NavBar';
+
 function App() {
   return (
     <div className="App">
+      
+    <NavBar/>
       <header className="App-header">
         <h1>Welcome to Sushi Switch!!</h1>
         <img src={logo} className="App-logo" alt="logo" />
